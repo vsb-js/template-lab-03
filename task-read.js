bf9c1a2e6fc2f237acce89e721aed60702f86a62
Petr Vecera <vecera.petr@gmail.com>
2022-09-14 15:20:15 +0200

    task read

--- task-read.js
+++ task-read.js
@@ -1,1 +1,1 @@
-// 1. display (console.log) all names of vehicles from local file vehicles.json
+// 1. display (console.log) all names of vehicles from local file data/vehicles.json (you can use FS readFileSync)
